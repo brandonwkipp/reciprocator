e5ea0a659e7bfbab410500d273bd403b96f0b810
Brandon W. Kipp <brandonwkipp@gmail.com>
2019-10-04 00:57:22 -0500

    refactor: Deleted editor, added basic select for key selection

--- src/Engine/index.js
+++ src/Engine/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FormGroup, Input } from 'reactstrap';
 
 const MidiParser = require('midi-parser-js');
 
@@ -7,10 +8,17 @@ class Engine extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      key: 0,
       json: {},
+      notes: [],
     };
 
+    this.findNegative = this.findNegative.bind(this);
     this.parse = this.parse.bind(this);
+  }
+
+  findNegative() {
+    const { key } = this.state;
   }
 
   parse() {
@@ -27,11 +35,33 @@ class Engine extends Component {
 
     return (
       <>
-        <input
-          id="filereader"
-          type="file"
-          onInput={() => this.parse()}
-        />
+        <FormGroup>
+          <input
+            id="filereader"
+            type="file"
+            onInput={() => this.parse()}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="select"
+            name="keySelect"
+            id="keySelect"
+          >
+            <option value={0}>C</option>
+            <option value={1}>C♯ / D♭</option>
+            <option value={2}>D</option>
+            <option value={3}>D♯ / E♭</option>
+            <option value={4}>E</option>
+            <option value={5}>F</option>
+            <option value={6}>F♯ / G♭</option>
+            <option value={7}>G</option>
+            <option value={8}>G♯ / A♭</option>
+            <option value={9}>A</option>
+            <option value={10}>A♯ / B♭</option>
+            <option value={11}>B</option>
+          </Input>
+        </FormGroup>
       </>
     );
   }
