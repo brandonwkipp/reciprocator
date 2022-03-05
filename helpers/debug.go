3d38b30bf7720ae79849ab538213be0bdd89ca73
Brandon Kipp <brandonwkipp@gmail.com>
2022-03-05 00:39:05 -0600

    refactor: Cleaned up helper funcs, added comments

--- helpers/debug.go
+++ helpers/debug.go
@@ -2,30 +2,44 @@ package helpers
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
+	"gitlab.com/gomidi/midi"
 	"gitlab.com/gomidi/midi/reader"
 	"gitlab.com/gomidi/midi/smf"
 )
 
-func DebugSMFFile(fileName string) {
+// DebugMisc prints out any message that is not a NoteOn/NoteOff message
+func DebugMisc(p *reader.Position, m midi.Message) {
+	if !strings.Contains(m.String(), "channel.Note") {
+		fmt.Printf("DEBUG MISC: %v\n", m)
+	}
+}
+
+// DebugNote prints out NoteOn/NoteOff messages
+func DebugNote(p *reader.Position, channel uint8, key uint8, velocity uint8) {
+	fmt.Printf("DEBUG NOTE: Position %v, Channel %v, Key %v, Velocity %v\n", p, channel, key, velocity)
+}
+
+// DebugSMF prints out the contents of a standard midi file
+func DebugSMF(fileName string) {
 	rd := reader.New(
 		reader.NoLogger(),
-		reader.NoteOn(DebugNote),
+		reader.Each(DebugMisc),
 		reader.NoteOff(DebugNote),
-		reader.SMFHeader(DebugSMFHeader),
+		reader.NoteOn(DebugNote),
 	)
+
 	err := reader.ReadSMFFile(rd, fileName)
 	if err != nil {
-		fmt.Printf("could not read SMF file %v\n", fileName)
+		log.Printf("could not read SMF file %v\n", fileName)
 		os.Exit(1)
 	}
 }
 
+// DebugSMFHeader prints out the contents of a standard midi file header
 func DebugSMFHeader(h smf.Header) {
 	fmt.Println(h)
-}
-
-func DebugNote(p *reader.Position, channel uint8, key uint8, velocity uint8) {
-	fmt.Printf("Note @ %v: Channel %v, Key %v, Velocity %v\n", p, channel, key, velocity)
 }
--- helpers/note.go
+++ helpers/note.go
@@ -6,18 +6,65 @@ import (
 	"errors"
 	"strings"
 
+	"gitlab.com/gomidi/midi"
 	"gitlab.com/gomidi/midi/reader"
 )
 
 //go:embed keymap.json
 var toneJson string
 var toneMap map[string]uint8
-var NoteEvents []NoteEvent
+var Events []interface{}
 
-// A NoteEvent contains all properties of any NoteOn/NoteOff messages rendered from an SMF
+// MiscEvent is a struct for capturing misc midi messages
+type MiscEvent struct {
+	Message  midi.Message
+	Position reader.Position
+}
+
+// NoteEvent is a struct for capturing midi note messages
 type NoteEvent struct {
 	Channel, Key, Velocity uint8
 	Position               reader.Position
+}
+
+// CaptureMiscMessage captures any message that is not a NoteOn/NoteOff message
+func CaptureMiscMessage(p *reader.Position, m midi.Message) {
+	if !strings.Contains(m.String(), "channel.Note") {
+		Events = append(Events, MiscEvent{
+			Message:  m,
+			Position: *p,
+		})
+	}
+}
+
+// CaptureNoteMessage captures NoteOn/NoteOff messages
+func CaptureNoteMessage(p *reader.Position, channel, key, velocity uint8) {
+	Events = append(Events, NoteEvent{
+		Channel:  channel,
+		Key:      key,
+		Velocity: velocity,
+		Position: *p,
+	})
+}
+
+// ClearEvents clears the Events slice
+func ClearEvents() {
+	Events = []interface{}{}
+}
+
+// InvertNote transposes a note to its mirror image with respect to the tonal center
+func InvertNote(key, tonalCenter uint8) uint8 {
+	// ignore the tonal center
+	if key == tonalCenter {
+		return key
+	}
+
+	// handle inversion based on distance from tonal center
+	if key > tonalCenter {
+		return tonalCenter - (key - tonalCenter) // transpose down
+	} else {
+		return tonalCenter + (tonalCenter - key) // transpose up
+	}
 }
 
 // LookupMidiKey returns the midi key value from keymap.json of a given tonal center
@@ -34,29 +81,5 @@ func LookupMidiKey(tonalCenter string) (uint8, error) {
 		return midiKey, nil
 	} else {
 		return 0, errors.New("could not find midi key for " + tonalCenter)
-	}
-}
-
-func NoteCapture(p *reader.Position, channel uint8, key uint8, velocity uint8) {
-	NoteEvents = append(NoteEvents, NoteEvent{
-		Channel:  channel,
-		Key:      key,
-		Velocity: velocity,
-		Position: *p,
-	})
-}
-
-// TransposeNote transposes a note to its mirror image
-func TransposeNote(key, tonalCenter uint8) uint8 {
-	// ignore the tonal center
-	if key == tonalCenter {
-		return key
-	}
-
-	// handle inversion based on distance from tonal center
-	if key > tonalCenter {
-		return tonalCenter - (key - tonalCenter) // transpose down
-	} else {
-		return tonalCenter + (tonalCenter - key) // transpose up
 	}
 }
