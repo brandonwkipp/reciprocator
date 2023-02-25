a7a6c8b39d03e0157e36303cce064fa58e08cd96
Brandon W. Kipp <brandonwkipp@gmail.com>
2023-02-24 20:20:44 -0600

    removed hardcoded tonal center

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::builder::ArgAction;
 use clap::Parser;
+use rimd::{note_num_to_name};
 
 mod reciprocator;
 use crate::reciprocator::file;
@@ -15,15 +16,19 @@ struct Args {
 
     #[arg(long, action = ArgAction::SetTrue)]
    invert: bool,
+
+   #[arg(long, short)]
+   tonal_center: u8,
 }
 
 fn main() {
     let args = Args::parse();
 
     if args.debug {
+        println!("Tonal center: {}", note_num_to_name(args.tonal_center as u32));
         file::debug_smf(&args.file);
         return;
     }
 
-    file::write_file(&args.file, 60, file::construct_output_filename(&args.file, args.invert), args.invert);
+    file::write_file(&args.file, args.tonal_center, file::construct_output_filename(&args.file, args.invert), args.invert);
 }
